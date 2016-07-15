2b904621f347096fe699dc57f58858f414a93cfd
pyler <pyler@azet.sk>
2016-07-15 18:54:28 +0200

    Release version

---
+++ app/src/main/java/com/pyler/betterbatterysaver/hooks/AutoSyncController.java
@@ -0,0 +1,26 @@
+package com.pyler.betterbatterysaver.hooks;
+
+import android.content.ContentResolver;
+
+import com.pyler.betterbatterysaver.util.Logger;
+import com.pyler.betterbatterysaver.util.Utils;
+
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class AutoSyncController {
+    public static final String TAG = "AutoSyncController";
+    public static final String KEY = "disable_auto_sync_control";
+
+    public static void init(XSharedPreferences prefs, XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!new Utils(null).shouldHook(prefs, lpparam, KEY)) return;
+
+        try {
+            XposedBridge.hookAllMethods(ContentResolver.class, "setMasterSyncAutomatically", XC_MethodReplacement.returnConstant(null));
+        } catch (Throwable t) {
+            Logger.i(TAG, t.getMessage());
+        }
+    }
+}
--- app/src/main/java/com/pyler/betterbatterysaver/hooks/XposedMain.java
+++ app/src/main/java/com/pyler/betterbatterysaver/hooks/XposedMain.java
@@ -21,22 +21,23 @@ public class XposedMain implements IXposedHookZygoteInit, IXposedHookLoadPackage
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
         if (!prefs.getBoolean("app_battery_saving", false)) return;
 
-        /*ActivityController.init(prefs, lpparam);
+        ActivityController.init(prefs, lpparam);
         AppActivityController.init(prefs, lpparam);
         AppBrightnessController.init(prefs, lpparam);
         AlarmController.init(prefs, lpparam);
         AppThemeController.init(prefs, lpparam);
+        AutoSyncController.init(prefs, lpparam);
         BluetoothController.init(prefs, lpparam);
         CameraController.init(prefs, lpparam);
-        ConnectionController.init(prefs, lpparam);*/
+        ConnectionController.init(prefs, lpparam);
         NotificationController.init(prefs, lpparam);
-        /*NotificationLightController.init(prefs, lpparam)
+        NotificationLightController.init(prefs, lpparam);
         SelfHook.init(prefs, lpparam);
         ServiceController.init(prefs, lpparam);
         VibrationController.init(prefs, lpparam);
         WakelockController.init(prefs, lpparam);
         WifiController.init(prefs, lpparam);
-        WifiApController.init(prefs, lpparam);*/
+        WifiApController.init(prefs, lpparam);
     }
 }
 
